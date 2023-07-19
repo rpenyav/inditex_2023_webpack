ba54fc9d328d8510bcb326a36d723a21e3177d73
Rafa Peña <rafa@rafapenya.com>
2023-07-20 01:29:13 +0200

    feat: first components

--- src/components/DetailViewEpisode.tsx
+++ src/components/DetailViewEpisode.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+// import GlobalLayout from "../../layout/GlobalLayout";
+// import ListViewPage from "../ListView/ListViewPage";
 
 const DetailViewEpisode: React.FC = () => {
-  return <>DetailViewEpisode</>;
+  return (
+    <>DetailView</>
+    // <GlobalLayout>
+    //   <ListViewPage />
+    // </GlobalLayout>
+  );
 };
 
 export default DetailViewEpisode;
