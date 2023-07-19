0699a1b99694377130c0c69b059084c436ad0701
Rafa Peña <rafa@rafapenya.com>
2023-07-19 21:05:57 +0200

    feat: created first components into /components

---
+++ src/components/DetailViewEpisode.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DetailViewEpisode: React.FC = () => {
+  return <>DetailViewEpisode</>;
+};
+
+export default DetailViewEpisode;
---
+++ src/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as HomePageView } from "./HomePageView";
+export { default as DetailViewPage } from "./DetailViewPage";
+export { default as DetailViewEpisode } from "./DetailViewEpisode";
