e78e2c1c269bad4734b1529d64f90ea5e2dbfadf
Rafa Peña <rafa@rafapenya.com>
2023-07-20 14:39:07 +0200

    chore: applied lazy load into router file

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { HomePageView, DetailViewPage, DetailViewEpisode } from "../components";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes as ReactRoutes,
-} from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//lazy load
+const HomePageView = lazy(() => import("../components/HomePageView"));
+const DetailViewPage = lazy(() => import("../components/DetailViewPage"));
+const DetailViewEpisode = lazy(() => import("../components/DetailViewEpisode"));
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
-      <ReactRoutes>
-        <Route path="/" element={<HomePageView />} />
-        <Route path="/podcast/:podcastId" element={<DetailViewPage />} />
-        <Route
-          path="/podcast/:podcastId/episode/:episodeId"
-          element={<DetailViewEpisode />}
-        />
-      </ReactRoutes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePageView />} />
+          <Route path="/podcast/:podcastId" element={<DetailViewPage />} />
+          <Route
+            path="/podcast/:podcastId/episode/:episodeId"
+            element={<DetailViewEpisode />}
+          />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
