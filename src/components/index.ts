ffec840c8ced18ead7f45bde4d1f461d86c44d10
Rafa Peña <rafa@rafapenya.com>
2023-07-20 14:13:11 +0200

    feat: reducer/action of detail podcast/episodes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
+import DetailViewPage from "./DetailViewPage";
+import DetailViewEpisode from "./DetailViewEpisode";
+import { Episode } from "../redux/detail/types";
 export { default as HomePageView } from "./HomePageView";
 export { default as DetailViewPage } from "./DetailViewPage";
 export { default as DetailViewEpisode } from "./DetailViewEpisode";
@@ -5,3 +8,4 @@ export { default as Cabecera } from "./Cabecera";
 export { default as SpinnerIcon } from "./SpinnerIcon";
 export { default as ListViewPage } from "./ListViewPage";
 export { default as PodcasterCardDetail } from "./PodcasterCardDetail";
+export { default as Episodes } from "./Episodes";
--- src/redux/detail/index.ts
+++ src/redux/detail/index.ts
@@ -1,3 +1,7 @@
+export * from "./detailAlbumReducer";
+export { fetchAlbumDetail } from "./detailAlbumActions";
 export * from "./types";
 export * from "./podcastByIdActions";
 export * from "./podcastByIdReducer";
+export * from "./episodesActions";
+export * from "./episodesReducer";
