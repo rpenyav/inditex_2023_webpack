8e861318bcbcfbb7f6c54e149a3502c291070aa0
Rafa Peña <rafa@rafapenya.com>
2023-07-20 02:10:21 +0200

    chore: modification for provider settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
-import store from "./redux/store";
-import { Provider } from "react-redux";
 
 export const App: React.FC = () => {
-  return (
-    <div>
-      <Provider store={store}>
-        <AppRoutes></AppRoutes>
-      </Provider>
-    </div>
-  );
+  return <AppRoutes></AppRoutes>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { App } from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
